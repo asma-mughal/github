a46d727e67c468b9517880bdc90842f851fa53d8
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2023-12-01 14:32:44 +0500

    DAY1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,87 +3,69 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import IconButton from '@mui/material/IconButton';
 import Popover from '@mui/material/Popover';
+import './navbar.scss';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 const Navbar = () => {
   
   return (
-    <div className="navbar h-16 bg-primary flex items-center text-sm text-gray-500">
-      <div className="wrapper w-full px-5 flex items-center justify-between">
-        <div className="search flex items-center border border-transparent p-1 mx-auto lg:w-1/2 xl:w-1/3">
-          <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg 
-           overflow-hidden">
-            <div className="grid place-items-center h-full w-12 bg-secondary-blue  text-gray-300">
-              <SearchOutlinedIcon fontSize="small" />
+    <div>
+  <div class="navbar h-50 border-b-0.5 border-gray-200 flex items-center text-red-500 text-sm">
+    <div class="wrapper w-full px-20 flex items-center justify-between">
+    <div class="search flex items-center border border-transparent p-3">
+    <form className="max-w-md flex items-center mx-auto bg-primary rounded-full px-4">
+            <div className="relative">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute top-0 bottom-0 w-4 h-4 my-auto text-gray-400 left-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                </svg>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="w-full py-2 h-full pl-12 pr-4 text-xs
+                    text-black bg-secondary-blue rounded-md outline-none"
+                />
             </div>
-
-            <input
-              className="peer h-full w-full bg-secondary-blue outline-none text-sm text-gray-700 pr-2"
-              type="text"
-              id="search"
-              placeholder="Search"
-            />
-          </div>
+        </form>
+        
+      </div>
+      <div class="items flex items-center">
+        
+        <div class="item flex items-center mr-2">
+        <span class="icon w-30 h-30 rounded-full p-1
+        border border-white text-white">
+    <ChatBubbleOutlineOutlinedIcon style={{ fontSize: 20 }} />
+    <span class="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"></span>
+  </span>
         </div>
-        <div className="items flex items-center">
-          <div className="item flex items-center mr-5 relative">
-            <div className="notification-dot absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></div>
-            <IconButton
-              color="inherit"
-             
-              style={{ border: '1px solid #ddd', borderRadius: '30%', padding: '5px' }}
-            >
-              <NotificationsNoneOutlinedIcon fontSize="small" style={{ color: 'white' }} />
-            </IconButton>
-            <Popover
-             
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-            >
-              <div className="p-2">You have new notifications!</div>
-            </Popover>
-          </div>
-          <div className="item flex items-center mr-5 relative">
-            <div className="notification-dot absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></div>
-            <IconButton
-              color="inherit"
-             
-              style={{ border: '1px solid #ddd', borderRadius: '30%', padding: '5px' }}
-            >
-              <ChatBubbleOutlineOutlinedIcon fontSize="small" style={{ color: 'white' }} />
-            </IconButton>
-            <Popover
-             
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-            >
-              <div className="p-2">You have new notifications!</div>
-            </Popover>
-          </div>
-          <div className="item flex items-center mr-5 relative">
-            <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-              className="avatar w-8 h-8 rounded-md"
-            />
-            {/* Add your notification counter component here */}
-          </div>
-          {/* Add other items/icons here */}
+        <div class="item flex items-center mr-2">
+        <span class="icon rounded-full p-1 border border-white text-white">
+    <NotificationsNoneOutlinedIcon style={{ fontSize: 20 }} />
+    <span class="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"></span>
+  
+  </span>
         </div>
-
+        <div class="item flex items-center relative">
+        <img
+        src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+        class="avatar w-30 h-30 max-w-full max-h-full rounded-full"
+    />
+          
+        </div>
       </div>
     </div>
+  </div>
+</div>
   );
 };
 
