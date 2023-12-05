e85b2bcdfbb64605dda01970f31ac20652a16688
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2023-12-05 14:06:02 +0500

    last commit

--- src/components/ComboBox.jsx
+++ src/components/ComboBox.jsx
@@ -24,16 +24,18 @@ export default function ComboExample() {
             .replace(/\s+/g, '')
             .includes(query.toLowerCase().replace(/\s+/g, ''))
         )
-
+      console.log(selected)
   return (
     <div className="bg-white">
       <Combobox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
-          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg 
+          bg-white text-left shadow-md focus:outline-none focus-visible:ring-2
+           focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
               displayValue={(person) => person.name}
-              onChange={(event) => setQuery(event.target.value)}
+              onChange={(event) => console.log(event.target.value)}
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
